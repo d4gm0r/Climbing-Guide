4697dfdb73f9026e02192cd13c3ca19911dc6116
Nathan Larsen <d4gm0r@gmail.com>
2017-04-17 03:25:13 +0000

    added 404 error

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,7 @@
+from django.http import Http404
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import get_object_or404, render
 from .models import Question
 
 # Create your views here.
@@ -12,7 +14,8 @@ def index(request):
     return render(request, 'polls/index.html', context)
     
 def detail(request, question_id):
-    return HttpResponse("Question: %s" % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question':question})
     
 def results(request, question_id):
     response = "Results for question %s:"
